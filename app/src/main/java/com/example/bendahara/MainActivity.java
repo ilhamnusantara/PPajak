82421c82d46e7a34253f3fc942f8746ee8f95446
ilhamnusantara <ilhamsdr@gmail.com>
2021-01-17 19:48:12 +0700

    first push

---
+++ app/src/main/java/com/example/bendahara/MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.bendahara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import static com.example.bendahara.R.id.nHasil;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nom, eKoma;
+    private TextView kalimat, nhasil;
+    private Button btDPP, btPPN, btPPh21,btPPh22,btPPh23,btPResto;
+    int angka;
+    int naik = 2;
+    double fnal,dpp,temp,ppn,pph21,pph22,pph23,presto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nom = findViewById(R.id.nHarga);
+        btDPP = findViewById(R.id.bDpp);
+        btPPN = findViewById(R.id.bPPN);
+        btPPh21 = findViewById(R.id.bPPh21);
+        btPPh22 = findViewById(R.id.bPPh22);
+        btPPh23 = findViewById(R.id.bPPh23);
+        btPResto = findViewById(R.id.bPResto);
+        kalimat = findViewById(R.id.Hasil);
+        nhasil = findViewById(R.id.nHasil);
+
+        btDPP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angka = Integer.parseInt(nom.getText().toString());
+                dpp = angka * 100 / 110;
+//                DecimalFormat twoDForm = new DecimalFormat("#.#");
+//                fnal = Double.parseDouble(twoDForm.format(dpp));
+//                BigDecimal bd = new BigDecimal(dpp);
+//                bd = bd.setScale(naik,BigDecimal.ROUND_UP);
+//                fnal = bd.doubleValue();
+                fnal = Math.round(dpp);
+                String coba = NumberFormat.getNumberInstance(Locale.US).format(fnal);
+                kalimat.setText("Nilai Dasar Pengenaan Pajak yang harus dibayar :");
+                nhasil.setText(""+coba);
+            }
+        });
+
+        btPPN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angka = Integer.parseInt(nom.getText().toString());
+                dpp = angka * 100 / 110;
+                ppn = dpp * 10 / 100;
+                fnal = Math.round(ppn);
+                String coba = NumberFormat.getNumberInstance(Locale.US).format(fnal);
+                kalimat.setText("Nilai Pajak Pertambahan Nilai yang harus dibayar :");
+                nhasil.setText(""+coba);
+            }
+        });
+
+        btPPh21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angka = Integer.parseInt(nom.getText().toString());
+            }
+        });
+
+        btPPh22.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angka = Integer.parseInt(nom.getText().toString());
+                dpp = angka * 100 / 110;
+                ppn = dpp * 10 / 100;
+                pph22 = ppn * 15 / 100;
+                fnal = Math.round(pph22);
+                String coba = NumberFormat.getNumberInstance(Locale.US).format(fnal);
+                kalimat.setText("Nilai Pajak Penghasilan Pasal 22 yang harus dibayar :");
+                nhasil.setText(""+coba);
+            }
+        });
+
+        btPPh23.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                angka = Integer.parseInt(nom.getText().toString());
+                pph23 = angka * 2 / 100;
+                fnal = Math.round(pph23);
+                String coba = NumberFormat.getNumberInstance(Locale.US).format(fnal);
+                kalimat.setText("Nilai Pajak Penghasilan Pasal 23 yang harus dibayar :");
+                nhasil.setText(""+coba);
+            }
+        });
+    }
+}
